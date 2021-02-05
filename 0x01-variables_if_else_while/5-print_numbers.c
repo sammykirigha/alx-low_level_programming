fb932a7a683db9def4456acb12fa9a3543439d87
sammykirigha <dkirigha18@gmail.com>
2021-02-05 13:13:50 +0000

    added file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Prints numbers from 0 to 10
+ * It has no arguments
+ *
+ * Return: 0 incase of no errors
+ */
+int main(void)
+{
+  printf("0123456789\n");
+  return (0);
+}
