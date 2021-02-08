d704c99519e3f3d66730113530560ee2d1400a64
sammykirigha <dkirigha18@gmail.com>
2021-02-08 18:38:07 +0000

    added a file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *main: entry point
+ *description: check is a character is lowercase
+ *return: 1 if lowercase and 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+int main(void)
+{
+	_islower('b');
+		return (0);
+}
