6fe90b8b2d50134ce4ab97aa590cd6770397edc4
sammykirigha <dkirigha18@gmail.com>
2021-02-09 11:13:17 +0000

    a fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -24,16 +24,3 @@ int print_sign(int n)
 		return (-1);
 	}
 }
-
-int main(void)
-{
-	int r;
-
-
-	r = print_sign(5);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
