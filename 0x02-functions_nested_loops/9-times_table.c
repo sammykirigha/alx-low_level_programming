8d2b9acb52a381d2ad8c8480cab2e8d9fd5759f8
sammykirigha <dkirigha18@gmail.com>
2021-02-09 00:12:09 +0000

    added a file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ *main: entry point
+ *description: print 9 timestable
+ *return: the values
+ */
+
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			product = x * y;
+			if (y != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (y == 0)
+			{
+				_putchar('0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if ((product < 10) && (y != 0))
+			{
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
